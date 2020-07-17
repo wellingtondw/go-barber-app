70a586dfc39cd1e4a916416587166b4a861a460f
Wellington <wellingtonslpbi@hotmail.com>
2020-07-17 20:22:01 -0300

    add logo to sign in page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Image } from 'react-native';
 import { Container } from './styles';
 
+import Logo from '../../assets/logo.png';
+
 const SignIn: React.FC = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Image source={Logo} />
+    </Container>
+  );
 };
 
 export default SignIn;
