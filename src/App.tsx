ee48ee21d72a76c8411f7ac0b029bd225bb90166
Wellington <wellingtonslpbi@hotmail.com>
2020-07-17 20:06:41 -0300

    add react navigation in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import { View, Text, StatusBar } from 'react-native'
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-    <StatusBar barStyle='light-content' backgroundColor='#312e38' />
-    <Text>Hello World</Text>
-  </View>
-}
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <Routes />
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
